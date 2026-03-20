40569faeed3da1f26b4c5cc9dcc6cce6f375ac50
Bhuvnesh <>
2026-03-20 08:35:41 +0400

    [FIX] Make use of the Lombok within an enum to simplify constructor

--- src/main/java/com/books/bonnets/librarian/utils/commons/Enums.java
+++ src/main/java/com/books/bonnets/librarian/utils/commons/Enums.java
@@ -1,22 +1,19 @@
 package com.books.bonnets.librarian.utils.commons;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 public class Enums {
 
+    @Getter
+    @AllArgsConstructor( access = lombok.AccessLevel.PRIVATE)//Set the enum constructor to private
     public enum Status {
         ORDERED((byte) 0),
         PAID((byte)1),
         CANCELLED((byte)2);
 
-        private final byte id;
-
-        Status(byte id) {
-            this.id = id;
-        }
-
         //Get id of the status
-        public byte getId() {
-            return id;
-        }
+        private final byte id;
 
         //Get the status from the id
         public static Status fromValue(byte id) {
